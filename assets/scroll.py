25a158ed6b05287140288e26a7a73fb311a43b38
Alec M <38357871+amattu2@users.noreply.github.com>
2019-09-25 11:21:06 -0400

    Minor revisions

--- assets/scroll.py
+++ assets/scroll.py
@@ -4,7 +4,7 @@ class ScrollFrame(tk.Frame):
 	def __init__(self, parent, background):
 		super().__init__(parent)
 
-		self.canvas = tk.Canvas(self, borderwidth=0, background=background, height = "1000")
+		self.canvas = tk.Canvas(self, borderwidth=0, background=background, bd=0, highlightthickness=0, relief='ridge', height = "1000")
 		self.viewPort = tk.Frame(self.canvas, background=background)
 		self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
 		self.canvas.configure(yscrollcommand=self.vsb.set)
